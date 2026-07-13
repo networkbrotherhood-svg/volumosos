ad63a9fbf64d427cb232edf364fdca7d594ca2df
networkbrotherhood-svg <networkbrotherhood@gmail.com>
2026-07-13 19:08:05 -0300

    Delete volumosos directory

--- volumosos/fix_app2.py
+++
@@ -1,8 +0,0 @@
-with open('src/App.tsx', 'r') as f:
-    content = f.read()
-
-content = content.replace('''csv += `${g.toUpperCase(            </ProtectedRoute>
-          )};${k.kpi};${k.comp};${k.real};${calcCopilNota(k)}\\n`;''', '''csv += `${g.toUpperCase()};${k.kpi};${k.comp};${k.real};${calcCopilNota(k)}\\n`;''')
-
-with open('src/App.tsx', 'w') as f:
-    f.write(content)
--- volumosos/fix_app4.py
+++
@@ -1,9 +0,0 @@
-with open('src/App.tsx', 'r') as f:
-    content = f.read()
-
-content = content.replace('''                      csv += `${g.toUpperCase(
-            </ProtectedRoute>
-          csv += `${g.toUpperCase()};${k.kpi};${k.comp};${k.real};${calcCopilNota(k)}\\n`;''', '''                      csv += `${g.toUpperCase()};${k.kpi};${k.comp};${k.real};${calcCopilNota(k)}\\n`;''')
-
-with open('src/App.tsx', 'w') as f:
-    f.write(content)
