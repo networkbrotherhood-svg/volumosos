ccbbd0a69e4a47a034f988d31c6216e37ec5528e
networkbrotherhood-svg <networkbrotherhood@gmail.com>
2026-07-13 19:45:43 -0300

    Delete src directory

--- src/types/COPIL.ts
+++
@@ -1,9 +0,0 @@
-import { KPI } from './KPI';
-
-export interface CopilSectorData {
-  setorId: string;
-  operacionais: KPI[];
-  economico: KPI[];
-  seguranca: KPI[];
-  lastUpdated: string;
-}
--- src/types/index.ts
+++
@@ -1,7 +0,0 @@
-export * from './Store';
-export * from './Radar';
-export * from './KPI';
-export * from './Setor';
-export * from './Usuario';
-export * from './OCR';
-export * from './COPIL';
